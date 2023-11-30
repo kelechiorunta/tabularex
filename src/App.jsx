277e8061eca86a2671b990ba123361fccbb731d8
kelechiorunta <kelechiorunta1@gmail.com>
2023-11-30 03:44:56 +0100

    chore: import dependencies for app component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/**
+ * Import React Dependencies
+ */
+import { useState } from 'react'
+
+/**
+ * Import Components
+ */
+import Row from './components/Row'
+import Header from './components/Header'
+
+import posts from './data/posts'
+import './App.css'
