b081a5eb8ad478eb03537989a4b18d335f189cd7
kelechiorunta <kelechiorunta1@gmail.com>
2023-11-30 03:37:25 +0100

    chore: a simple data posts store for testing

---
+++ src/data/posts.js
@@ -0,0 +1,27 @@
+/**
+ * Posts
+ *
+ * A simple posts data store.
+ */
+export const posts = [
+  {
+    id: 1,
+    title: "Hello World!",
+    content: "What a Wonderful World!",
+  },
+  {
+    id: 2,
+    title: "In a land of Magic",
+    content: "The destiny of a great King depended on Merlin!",
+  },
+  {
+    id: 3,
+    title: "The Lion & the Jewel",
+    content: "I forbid you to come into the King's court, forever!",
+  },
+  {
+    id: 4,
+    title: "A tale of 2 Cities",
+    content: "When I come into my dwelling, I shall find ye!",
+  },
+];
