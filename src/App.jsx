e5ae697ea10d417679af7f159e898ca7714e96b2
kelechiorunta <kelechiorunta1@gmail.com>
2023-12-02 21:06:38 +0100

    chore: App.jsx updates

--- src/App.jsx
+++ src/App.jsx
@@ -27,19 +27,29 @@ const App = () => {
    * Declaration of constants and variables and hooks for the application
    */
   var newpost = [];
+  var post_paginate = [];
+  var searcheddata = [];
+  var lastIndex=5, firstIndex=0;
+  //var mystyle;
+
+  const samplepost = Array.from({length:20},(item, i)=>i+1)
   const [data, setData] = useState([...posts])
-  const [sampledata, setSample] = useState([])
+  const [mystyle, setmystyle] = useState({})
+  const [searchedId, setsearchedId] = useState("")
+  // const [search_val, setValue] = useState(1)
+  // const [sampledata, setSample] = useState([...samplepost])
   const [currentPage, setcurrentPage] = useState(1);
   const [order, setorder] = useState("ASC")
   const NumberofPages = 5;
+  // const samplepost = Array.from({length:20},(item, i)=>i+1)
 
 /**
  * Declaration of the user function for Pagination
  */
   const Paginate = () => {
-    const lastIndex = currentPage * NumberofPages;
-    const firstIndex = lastIndex - NumberofPages;
-    const post_paginate = posts.slice(firstIndex, lastIndex)
+    lastIndex = currentPage * NumberofPages;
+    firstIndex = lastIndex - NumberofPages;
+    post_paginate = posts.slice(firstIndex, lastIndex)
     post_paginate.length===0? <h1>No Data Found</h1> : setData([...post_paginate])
   }
 
@@ -49,8 +59,21 @@ const App = () => {
  */
   const sorting = (sortProp) => {
     order==="ASC"? (newpost = data.sort((a,b)=>{ return a[sortProp.toLowerCase()] < b[sortProp.toLowerCase()]?
-    1:-1 }),setData([...newpost]), setorder("DSC")) :(newpost = data.sort((a,b)=>{return a[sortProp.toLowerCase()]
+    1:-1 },setData([...newpost]), setorder("DSC"))) :(newpost = data.sort((a,b)=>{return a[sortProp.toLowerCase()]
     < b[sortProp.toLowerCase()]? -1:1}), setData([...newpost]), setorder("ASC"))
+  }
+
+  /**
+   * Declaration of the user function for searching and selecting the item based on the input value or id
+   * automatically loding in the current page triggered by the useEffect hook with the currentPage aa a dependant value
+   * @param {*} e
+   */
+  const searchInput = (e) =>{
+
+    e.target.value===""? (Paginate(), setsearchedId("")):(setcurrentPage(Math.ceil(Number(e.target.value)/5)),(setsearchedId(Number(e.target.value))))
+    // (searcheddata = posts.filter((item, index)=>{return item.id === Number(e.target.value)}),(setmyclass(e.target.value)),
+    // setData([...searcheddata]));
+
   }
 
   /**
@@ -70,17 +93,22 @@ const App = () => {
           <Header sorting={sorting} />
 
            { data.map((post, id) => {
-             return <Row key={id}  id={post.id} title={post.title} content={post.body}/>
+             return <Row key={id}  id={post.id} title={post.title} content={post.body} selectstyle={mystyle} searchedrow={searchedId}/>
             })
           }
 
-          <fieldset className="page_container"> <span>Page:</span><input type="Number" min={1} max={posts.length/5}
-           value={currentPage} onChange={(e)=>{ setcurrentPage((e.target.value)); Paginate();}}/></fieldset>
-
+          <fieldset className="page_container">
+            <fieldset className='search_id_cont'><span>Search ID:</span><input className='search_input' onChange={searchInput} type="text"/></fieldset>
+            <ul className='page_link'>
+              <button onClick={()=>(currentPage > 1) && setcurrentPage(prev=> prev-1)}><a href="#">Prev</a></button>
+              {<input className="pageno" type="Number" min={1} max={posts.length/5}
+              value={currentPage} onChange={(e)=> (setcurrentPage(Number(e.target.value)), Paginate())}/>}
+              <button onClick={()=>(currentPage < posts.length/5) && setcurrentPage(prev=>prev+1)}><a href="#">Next</a></button>
+            </ul>
+          </fieldset>
         </div>
       </section>
     </>
   )
-}
-
-export default App;
+          }
+export default App
