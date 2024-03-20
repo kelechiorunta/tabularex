9c26cb851330daf5b9941714c5d0368839802424
kelechiorunta <kelechiorunta1@gmail.com>
2024-03-20 18:35:13 +0100

    Remove unused code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,6 @@ import App from '../App';
 const Header = ({sorting, ascending_id, descending_id, ascending_title, descending_title, ascending_cont, descending_cont}) => {
   return (
     <ul className="header">
-      {/* <li>USER ID</li> */}
       <li onClick={()=> sorting("id")}>ID </li>
       <li onClick={()=> sorting("title")}>Title</li>
       <li onClick={()=> sorting("body")}>Content</li>
@@ -21,11 +20,3 @@ const Header = ({sorting, ascending_id, descending_id, ascending_title, descendi
 }
 
 export default Header;
-
-
-          // <span><fieldset><legend>Sort</legend><button onClick={ascending_id}>A</button>
-          // <button onClick={descending_id}>D</button></fieldset></span>
-          // <span><fieldset><legend>Sort</legend><button onClick={ascending_title}>A</button>
-          // <button onClick={descending_title}>D</button></fieldset></span>
-          // <span><fieldset><legend>Sort</legend><button onClick={ascending_cont}>A</button>
-          // <button onClick={descending_cont}>D</button></fieldset></span>
