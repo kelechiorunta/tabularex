34015e861ef769145b7862b6989735d88dbf6c1c
kelechiorunta <kelechiorunta1@gmail.com>
2024-03-20 21:56:13 +0100

    chore: fix linting issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,10 +11,9 @@ import App from '../App';
 const Header = ({sorting, ascending_id, descending_id, ascending_title, descending_title, ascending_cont, descending_cont}) => {
   return (
     <ul className="header">
-      <li onClick={()=> sorting("id")}>ID </li>
+      <li onClick={()=> sorting("id")}>ID</li>
       <li onClick={()=> sorting("title")}>Title</li>
       <li onClick={()=> sorting("body")}>Content</li>
-
     </ul>
   );
 }
